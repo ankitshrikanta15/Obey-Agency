793625926f0c07b6385c1a1ffbbfaf896b21c660
ankitshrikanta15 <ankitshrikanta@gmail.com>
2024-06-06 18:50:50 +0530

    page1 created

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let tl = gsap.timeline();
+function loadingAnimation() {
+  let tl = gsap.timeline();
 
 tl.from(".line h1", {
   y: 150,
@@ -32,17 +33,43 @@ tl.to(".line h2",{
 tl.to(".loader", {
   opacity: 0,
   duration: 0.2,
-  delay: 3.9,
+  // delay: 3.9,
+  delay: 0
 });
 
 tl.from(".page1", {
   delay: 0.2,
   y: 1600,
   opacity: 0,
+  duration: 0.5,
   ease: Power4,
-  duration: 0.5 
 });
 
 tl.to(".loader", {
   display: "none",
 });
+
+tl.from("nav",{
+  opacity: 0
+})
+
+tl.from(".underline-hero1 h1, .underline-hero2 h1, .underline-hero3 h2, .underline-hero3 h3, .underline-hero4 h1",{
+  y: 120,
+  stagger: 0.2
+})
+
+}
+
+function cursorAnimation() {
+  document.addEventListener('mousemove', function(dets){
+    gsap.to(".cursor",{
+      left: dets.x,
+      top: dets.y
+      });
+      });
+      
+      Shery.makeMagnet(".nav-part2 h4");
+      }
+
+loadingAnimation();
+cursorAnimation();
